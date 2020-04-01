80a0b2ec9f5f042ed8c7955266f777baaf086ef1
ThomasThorntonHal <43864737+ThomasThorntonHal@users.noreply.github.com>
2020-04-01 13:44:40 +0100

    Implemented LinkedStack

--- src/projectCode20280/LinkedStack.java
+++ src/projectCode20280/LinkedStack.java
@@ -2,39 +2,95 @@ package projectCode20280;
 
 public class LinkedStack<E> implements Stack<E> {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    /** Storage for elements in stack */
+    private SinglyLinkedList<E> list = new SinglyLinkedList<>(); // an empty list
 
-	}
+    public LinkedStack() { } // new stack relies on the initially empty list
 
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        LinkedStack<Integer> stack = new LinkedStack<>();
 
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.push(60);
+        stack.push(70);
+        stack.push(80);
+        stack.push(90);
+        stack.push(100);
+        stack.push(110);
 
-	@Override
-	public void push(E e) {
-		// TODO Auto-generated method stub
-		
-	}
+        System.out.println("stack size: " + stack.size());
+        System.out.println(stack);
 
-	@Override
-	public E top() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        System.out.println( stack.pop() );
+        System.out.println( stack.pop() );
+        System.out.println( stack.pop() );
 
-	@Override
-	public E pop() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        System.out.println( stack );
 
+    }
+
+    /**
+     * @return number of elements in the stack
+     */
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    /**
+     * @return true if stack is empty
+     */
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    /**
+     * Adds element to top of the stack
+     *
+     * @param e element to be added
+     */
+    @Override
+    public void push(E e) {
+        list.addFirst(e);
+    }
+
+    /**
+     * Returns but does not delete the top element on the stack
+     *
+     * @return top element of the stack
+     */
+    @Override
+    public E top() {
+        return list.first();
+    }
+
+    /**
+     * Returns and deletes top element form the stack
+     *
+     * @return deleted element
+     */
+    @Override
+    public E pop() {
+        return list.removeFirst();
+    }
+
+    @Override
+    public String toString() {
+        String result = "[";
+
+        for(int i = 0; i < size(); i++){
+            result += list.get(size() - i - 1); /* this toString method prints the array backwards
+                                                    just to keep consistency with ArrayStack */
+            if(i < size() - 1){
+                result += ", ";
+            }
+        }
+        return result + "]";
+    }
 }
